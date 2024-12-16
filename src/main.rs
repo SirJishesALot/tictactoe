860746c2f04227a259e2845575295149209bfc9d
Jishnu Raychaudhuri <jira5329@colorado.edu>
2024-12-15 23:07:45 -0800

    doesn't grow on error

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod tictactoe;
 
 use tictactoe::{TicTacToe, Mark}; 
 use colored::*; 
-use termion::{clear, cursor}; 
+use crossterm::{execute, terminal, cursor};
 
 macro_rules! print_flush {
     ($($arg:tt)*) => {
@@ -14,11 +14,25 @@ macro_rules! print_flush {
     };
 }
 
+fn error_msg(message: &str, position: (u16, u16)) {
+    clear_line();
+    println!("{}", message.red()); 
+    move_cursor(position);
+    clear_line(); 
+}
+
+fn move_cursor(position: (u16, u16)) {
+    execute!(std::io::stdout(), cursor::MoveTo(position.0, position.1)).unwrap();
+}
+
+fn clear_line() {
+    execute!(std::io::stdout(), terminal::Clear(terminal::ClearType::CurrentLine)).unwrap(); 
+}
+
 fn clear_screen() {
-    use std::io::Write;
-    print!("{}", clear::All); 
-    print!("{}", cursor::Goto(1, 1));
-    std::io::stdout().flush().unwrap();
+    use std::io; 
+    execute!(io::stdout(), terminal::Clear(terminal::ClearType::All)).unwrap(); 
+    move_cursor((0, 0));
 }
 
 fn display_rules() {
@@ -27,11 +41,12 @@ fn display_rules() {
     println!("Player 1 puts down '{}'s on the board while Player 2 puts down '{}'s on the board.", Mark::X, Mark::O);
     println!("A player wins when they form a straight line with their respective mark, '{}' or '{}', first.", Mark::X, Mark::O);
     println!("{}", "========================= Instructions ==========================".black().on_white()); 
-    println!("1.    Enter the {} in the grid to place your mark in that position.", "letter".bold()); 
-    println!("2.    The first player to make a straight line with their mark wins the game.");
+    println!("  1. Enter the {} in the grid to place your mark in that position.", "letter".bold()); 
+    println!("  2. The first player to make a straight line with their mark wins the game.");
 }
 
 fn get_grid_size() -> usize {
+    let position = cursor::position().unwrap(); 
     loop {
         let mut size_str = String::new(); 
         print_flush!("Enter the size of the grid (2 <= size <= 7): "); 
@@ -39,8 +54,8 @@ fn get_grid_size() -> usize {
 
         match size_str.trim().parse::<usize>() {
             Ok(size) if size >= 2 && size <= 7 => break size, 
-            Ok(_) => println!("Grid size must be within the specified bounds."), 
-            Err(_) => println!("Please enter a valid number."),
+            Ok(_) => error_msg("Grid size must be within the specified bounds.", position),
+            Err(_) => error_msg("Please enter a valid number", position),
         }
     }
 }
@@ -49,6 +64,7 @@ fn main() {
     use std::io; 
     clear_screen();
     println!("Welcome to Tic Tac Toe!");
+    let start_position = cursor::position().unwrap(); 
     loop {
         print_flush!("Would you like to read the rules and instructions? (y/n): ");
         let mut rule_choice = String::new(); 
@@ -64,7 +80,11 @@ fn main() {
                 break; 
             }, 
             "n" | "no" => break, 
-            _ => continue,
+            _ => {
+                move_cursor(start_position);
+                clear_line(); 
+                continue
+            },
         };
     }
 
@@ -80,17 +100,18 @@ fn main() {
             clear_screen();
             game.display_board(); 
             println!("\nIt's Player {}'s turn ({}).", (turn % 2) + 1, marks[turn % 2]); 
-
+            
+            let position = cursor::position().unwrap(); 
             let cell = loop {
                 print_flush!("Enter a cell: "); 
                 let mut cell_str = String::new(); 
                 io::stdin().read_line(&mut cell_str).expect("Failed to read line."); 
 
                 if cell_str.trim().len() > 1 {
-                    println!("Please enter a single character");
+                    error_msg("Please enter a single character", position);
                 } else if let Some(ch) = cell_str.trim().chars().next() {
                     if !game.check_valid_cell(ch) {
-                        println!("Please enter a valid cell."); 
+                        error_msg("Please enter a valid cell.", position); 
                     } else { break ch; }
                 }
             };
